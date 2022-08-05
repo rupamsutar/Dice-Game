6b4d75106383340dbc2cc3a3fcac4c2e5d1e6911
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-05 17:32:06 +0530

    Added random images with js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-var randomNumber1 = 7 * (Math.random());
+var randomNumber1 = Math.floor((6 * (Math.random())) + 1);
+document.querySelector("img").setAttribute("src", ("images/dice"+ randomNumber1 + ".png"));
+var randomNumber2 = Math.floor((6 * (Math.random())) + 1);
+document.querySelector(".img2").setAttribute("src", ("images/dice" + randomNumber2 + ".png"));
