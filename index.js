bef2ee30250bafb857ad67bd86254c3cff180915
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-05 18:08:40 +0530

    Added the result using JS

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ var randomNumber1 = Math.floor((6 * (Math.random())) + 1);
 document.querySelector("img").setAttribute("src", ("images/dice"+ randomNumber1 + ".png"));
 var randomNumber2 = Math.floor((6 * (Math.random())) + 1);
 document.querySelector(".img2").setAttribute("src", ("images/dice" + randomNumber2 + ".png"));
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+} else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+} else {
+  document.querySelector("h1").innerHTML = "Its a Draw ! ";
+}
